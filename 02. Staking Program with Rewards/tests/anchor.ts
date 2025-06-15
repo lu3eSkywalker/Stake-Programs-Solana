37bdfca7376062e61c5448bd81594bf5b099a33f
lu3eSkywalker <surajjhajhria1@gmail.com>
2025-06-16 00:14:23 +0530

    adding test claim points test case and fixing getting rewards test case

--- 02. Staking Program with Rewards/tests/anchor.ts
+++ 02. Staking Program with Rewards/tests/anchor.ts
@@ -125,32 +125,58 @@ describe("Test", () => {
     assert.equal(userAccountUpdate.stakedAmount.toNumber(), 3000000000);
   });
   it("should get the reward points", async () => {
-    const [pdaAccount, bump2] = await web3.PublicKey.findProgramAddress(
+    const [pdaAccount, bump] = await web3.PublicKey.findProgramAddress(
       [Buffer.from("client1"), program.provider.publicKey.toBuffer()],
       program.programId
     );
     const userAccount = await program.account.stakeAccount.fetch(pdaAccount);
-
     console.log(
       "User Staked Amount: ",
       userAccount.stakedAmount.toNumber() / 1000000000,
       "Sol"
     );
-
+    console.log(
+      "These are the reward points of user: ",
+      userAccount.totalPoints.toNumber()
+    );
     console.log("This is the current time: ", Math.floor(Date.now() / 1000));
-
     const current_time = Math.floor(Date.now() / 1000);
-
     const user_staked_sol_amount =
       userAccount.stakedAmount.toNumber() / 1000000000;
     const last_update_time = userAccount.lastUpdateTime.toNumber();
-
     const time_elapsed = current_time - last_update_time;
-
     const RewardPoints = user_staked_sol_amount * time_elapsed;
 
-    const newRewardPoints = RewardPoints + userAccount.totalPoints.toNumber();
-
-    console.log("The latest reward point of the user is: ", newRewardPoints);
+    if (userAccount.totalPoints.toNumber() == 0) {
+      console.log("The updated reward point of the user is: ", RewardPoints);
+    } else {
+      const newRewardPoints = RewardPoints + userAccount.totalPoints.toNumber();
+      console.log("The updated reward point of the user is: ", newRewardPoints);
+    }
+  });
+  it("should reset the points", async () => {
+    const [pdaAccount, bump] = await web3.PublicKey.findProgramAddress(
+      [Buffer.from("client1"), program.provider.publicKey.toBuffer()],
+      program.programId
+    );
+    // Send Transction
+    const txHash = await program.methods
+      .claimPoints()
+      .accounts({
+        payer: program.provider.publicKey,
+        pdaAccount: pdaAccount,
+        systemProgram: web3.SystemProgram.programId,
+      })
+      .rpc();
+    console.log(`Use 'solana confirm -v ${txHash}' to see the logs`);
+    // Confirm Transaction
+    await program.provider.connection.confirmTransaction(txHash);
+    const account = await program.account.stakeAccount.fetch(pdaAccount);
+    console.log(
+      "These are the reward points of user after claiming points: ",
+      account.totalPoints.toNumber()
+    );
+    // Assertions
+    assert.equal(account.totalPoints.toNumber(), 0);
   });
 });
